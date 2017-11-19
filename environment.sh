69580496f0abc17b0cea6b491eff2c6042fba89a
David R. Piegdon <dgit@piegdon.de>
2017-11-19 00:16:01 +0000

    cleanup environment.sh script

--- environment.sh
+++ environment.sh
@@ -2,19 +2,6 @@
 
 echo "reminder: always source this script, not simply execute it."
 echo "setting up build environment..."
-for newpath in	arachne-pnr/bin \
-		icestorm/icebox \
-		icestorm/icebram \
-		icestorm/icecompr \
-		icestorm/icefuzz \
-		icestorm/icemulti \
-		icestorm/icepack \
-		icestorm/icepll \
-		icestorm/iceprog \
-		icestorm/icetime \
-		yosys \
-		; do
-	newfullpath="`pwd`/$newpath"
-	echo "$PATH" | grep "$newpath" > /dev/null || export PATH="$newfullpath:$PATH"
-done
-
+newpath="build/bin"
+newfullpath="`pwd`/$newpath"
+echo "$PATH" | grep "$newpath" > /dev/null || export PATH="$newfullpath:$PATH"

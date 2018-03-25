10b93d12bc4483f1d447f02eb447ebd7c3d8d1c5
David R. Piegdon <dgit@piegdon.de>
2018-03-25 00:10:47 +0000

    fix environment.sh to work from other paths

--- environment.sh
+++ environment.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 echo "reminder: always source this script, not simply execute it."
-echo "setting up build environment..."
+echo "setting up build environment:"
+
 newpath="build/bin"
-newfullpath="`pwd`/$newpath"
+newfullpath="`dirname $0`/$newpath"
 echo "$PATH" | grep "$newpath" > /dev/null || export PATH="$newfullpath:$PATH"
+echo "PATH += $newfullpath"

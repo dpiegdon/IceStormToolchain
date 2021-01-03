5be4b46ba9922f434f423d0544fb4f075b641274
David R. Piegdon <dgit@piegdon.de>
2021-01-03 04:59:04 +0000

    update upstream; improve update-script

--- update_all_repos.sh
+++ update_all_repos.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-for R in arachne-pnr icestorm nextpnr prjtrellis yosys; do
-	cd $R
+for R in $(git submodule status | awk '{print $2}'); do
+	echo "updating submodule: $R"
+	cd "$R"
 	git checkout master
 	git pull --rebase
 	git submodule update
